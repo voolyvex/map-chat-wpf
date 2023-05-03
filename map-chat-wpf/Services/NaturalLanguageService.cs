91005c6e378b5c0a8ff0857bcbfaee562e210cc8
voolyvex <1joeknapp@gmail.com>
2023-05-03 16:23:39 -0500

    Refactored NaturalLanguageService.cs to be more focused on specific tasks

--- map-chat-wpf/Services/NaturalLanguageService.cs
+++ map-chat-wpf/Services/NaturalLanguageService.cs
@@ -1,83 +1,110 @@
-using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
 using Esri.ArcGISRuntime.Symbology;
 using Esri.ArcGISRuntime.UI;
-using map_chat_wpf.Services;
+using Newtonsoft.Json;
+
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 
-public class NaturalLanguageService
+public interface INaturalLanguageService
+{
+    Task<string> ProcessMessageAsync(string message);
+}
+
+public class LuisNaturalLanguageService : INaturalLanguageService
+{
+    private readonly LuisClient _client;
+
+    public LuisNaturalLanguageService(LuisClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<string> ProcessMessageAsync(string message)
+    {
+        var response = await _client.PredictAsync(message);
+
+        // Get the top-scoring intent and extract any relevant entities
+        var topIntent = response.Prediction.TopIntent;
+        switch (topIntent.Intent)
+        {
+            case "ShowMapPoints":
+                // Extract any locations from the user's message
+                var locations = response.Prediction.Entities
+                    .Where(entity => entity.Type == "Location")
+                    .Select(entity => entity.Text)
+                    .ToList();
+
+                if (locations.Count == 0)
+                {
+                    return "Please specify a location.";
+                }
+
+                // Build a list of points from the locations
+                var builder = new MapPointsBuilder();
+                var points = builder.BuildMapPoints(string.Join(",", locations));
+                if (points.Count == 0)
+                {
+                    return "No points found for the specified location.";
+                }
+
+                // Update the map display with the points
+                UpdateMapDisplay(points);
+                return "Showing points for the specified location.";
+            // Add more cases for other intents here
+            default:
+                return "I'm sorry, I don't understand.";
+        }
+    }
+}
+
+public class LuisClient
 {
     private const string LUIS_APP_ID = "<your LUIS app ID>";
     private const string LUIS_SUBSCRIPTION_KEY = "<your LUIS subscription key>";
     private const string LUIS_ENDPOINT = "https://<your LUIS endpoint>.api.cognitive.microsoft.com/";
 
+    private readonly HttpClient _httpClient;
+
+    public LuisClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        _httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", LUIS_SUBSCRIPTION_KEY);
+    }
+
+    public async Task<LUISResponse> PredictAsync(string message)
+    {
+        var uri = $"{LUIS_ENDPOINT}luis/prediction/v3.0/apps/{LUIS_APP_ID}/slots/production/predict?query={message}";
+        var response = await _httpClient.GetAsync(uri);
+        response.EnsureSuccessStatusCode();
+        var responseContent = await response.Content.ReadAsStringAsync();
+        return JsonSerializer.Deserialize<LUISResponse>(responseContent);
+    }
+}
+
+public class MapPointsBuilder
+{
+    public List<Point> BuildMapPoints(string userInput)
+    {
+        // Build a list of points from the user's input
+        // ...
+
+        return new List<Point>();
+    }
+}
+
+public class MapDisplayUpdater
+{
     private readonly Map _map;
 
-    public NaturalLanguageService(Map map)
+    public MapDisplayUpdater(Map map)
     {
         _map = map;
     }
 
-    public async Task<string> ProcessMessageAsync(string message)
-    {
-        // Send a request to LUIS to get the predicted intent and entities from the user's message
-        using (HttpClient client = new HttpClient())
-        {
-            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", LUIS_SUBSCRIPTION_KEY);
-            string uri = $"{LUIS_ENDPOINT}luis/prediction/v3.0/apps/{LUIS_APP_ID}/slots/production/predict?query={message}";
-            HttpResponseMessage response = await client.GetAsync(uri);
-            if (!response.IsSuccessStatusCode)
-            {
-                return "Error: Failed to get response from LUIS";
-            }
-
-            string responseContent = await response.Content.ReadAsStringAsync();
-            LUISResponse luisResponse = JsonSerializer.Deserialize<LUISResponse>(responseContent);
-
-            // Get the top-scoring intent and extract any relevant entities
-            LUISIntent topIntent = luisResponse.Prediction.TopIntent;
-            string intent = topIntent.Intent;
-            switch (intent)
-            {
-                case "ShowMapPoints":
-                    List<string> locations = new List<string>();
-                    foreach (var entity in luisResponse.Prediction.Entities)
-                    {
-                        if (entity.Type == "Location")
-                        {
-                            locations.Add(entity.Text);
-                        }
-                    }
-                    if (locations.Count == 0)
-                    {
-                        return "Please specify a location.";
-                    }
-                    else
-                    {
-                        string userInput = string.Join(",", locations);
-                        MapPointsBuilder builder = new MapPointsBuilder();
-                        List<Point> points = builder.BuildMapPoints(userInput);
-                        if (points.Count == 0)
-                        {
-                            return "No points found for the specified location.";
-                        }
-                        else
-                        {
-                            UpdateMapDisplay(points);
-                            return "Showing points for the specified location.";
-                        }
-                    }
-                // Add more cases for other intents here
-                default:
-                    return "I'm sorry, I don't understand.";
-            }
-        }
-    }
-
-    private void UpdateMapDisplay(List<Point> points)
+    public void UpdateMapDisplay(List<Point> points)
     {
         // Create a graphics overlay to hold the graphics
         GraphicsOverlay overlay = new GraphicsOverlay();
@@ -93,34 +120,4 @@ public class NaturalLanguageService
         // Add the overlay to the map
         _map.OperationalLayers.Add(overlay);
     }
-}
-
-public class LUISResponse
-{
-    public LUISPrediction Prediction { get; set; }
-}
-
-public class LUISPrediction
-{
-    public LUISIntent TopIntent { get; set; }
-    public List<LUISEntity> Entities { get; set; }
-}
-
-public class LUISIntent
-{
-    public string Intent { get; set; }
-}
-
-public class LUISEntity
-{
-    public string Type { get; set; }
-    public string Text { get; set; }
-}
-
-
-public class NaturalLanguageService
-{
-    private const string LUIS_APP_ID = "<your LUIS app ID>";
-    private const string LUIS_SUBSCRIPTION_KEY = "<your LUIS subscription key>";
-    private const string LUIS_ENDPOINT = "https://<your LUIS endpoint>.api.cognitive.microsoft.com/";
 }
