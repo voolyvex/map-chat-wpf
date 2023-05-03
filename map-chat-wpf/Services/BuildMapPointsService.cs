fad0f909b8e5744ef34ec239873498aa0f53c815
voolyvex <1joeknapp@gmail.com>
2023-05-03 15:46:03 -0500

    feat: added MapPointsBuilder class in Services + moved MapViewModel into Services

---
+++ map-chat-wpf/Services/BuildMapPointsService.cs
@@ -0,0 +1,29 @@
+﻿using Esri.ArcGISRuntime.Geometry;
+using System.Collections.Generic;
+
+namespace map_chat_wpf.Services
+{
+    public class MapPointsBuilder
+    {
+        public List<Point> BuildMapPoints(string userInput)
+        {
+            List<Point> points = new List<Point>();
+
+            // Parse the user input to extract point coordinates and create Point objects
+            // You can add your own implementation here to parse the user input in the way you need
+            // Here's an example of how you might parse a comma-separated string of coordinates
+            string[] coordinates = userInput.Split(',');
+            foreach (string coordinate in coordinates)
+            {
+                string[] values = coordinate.Trim().Split(' ');
+                double x, y;
+                if (values.Length == 2 && double.TryParse(values[0], out x) && double.TryParse(values[1], out y))
+                {
+                    points.Add(new Point(x, y, SpatialReferences.Wgs84));
+                }
+            }
+
+            return points;
+        }
+    }
+}
---
+++ map-chat-wpf/Services/MapViewModel.cs
@@ -0,0 +1,69 @@
+﻿using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.Symbology;
+using Esri.ArcGISRuntime.UI;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace map_chat_wpf.Services
+{
+    internal class MapViewModel : INotifyPropertyChanged
+    {
+        public MapViewModel()
+        {
+            SetupMap();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private Map? _map;
+        public Map? Map
+        {
+            get { return _map; }
+            set
+            {
+                _map = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void SetupMap()
+        {
+
+            // Create a new map with a 'topographic vector' basemap.
+            Map = new Map(BasemapStyle.ArcGISTopographic);
+
+        }
+
+        public void UpdateMapDisplay(string userInput)
+        {
+            // Create a new graphics overlay to display the points.
+            GraphicsOverlay overlay = new GraphicsOverlay();
+
+            // Build the list of points from the user input using the MapPointsBuilder class.
+            MapPointsBuilder builder = new MapPointsBuilder();
+            List<Point> points = builder.BuildMapPoints(userInput);
+
+            // Add a graphic for each point.
+            foreach (Point point in points)
+            {
+                // Create a symbol for the point.
+                SimpleMarkerSymbol symbol = new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, System.Drawing.Color.Red, 10);
+
+                // Create a graphic for the point with the symbol and geometry.
+                Graphic graphic = new Graphic(point, symbol);
+
+                // Add the graphic to the overlay.
+                overlay.Graphics.Add(graphic);
+            }
+
+            // Add the overlay to the map's operational layers.
+            Map.OperationalLayers.Add(overlay);
+        }
+
+    }
+}
