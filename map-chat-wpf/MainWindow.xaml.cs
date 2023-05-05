5256c3630687388690849e51fd7bed37c7b59bf6
voolyvex <1joeknapp@gmail.com>
2023-05-05 13:22:18 -0500

    feat: added UI for user message inputs

--- map-chat-wpf/MainWindow.xaml.cs
+++ map-chat-wpf/MainWindow.xaml.cs
@@ -1,20 +1,50 @@
 ﻿using Esri.ArcGISRuntime.Geometry;
 using Esri.ArcGISRuntime.Mapping;
+using System.ComponentModel;
 using System.Windows;
+using System.Windows.Input;
 
 namespace map_chat_wpf
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _messageText;
+
+        public string MessageText
+        {
+            get { return _messageText; }
+            set
+            {
+                _messageText = value;
+                NotifyPropertyChanged(nameof(MessageText));
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
 
+            DataContext = this;
+
             MapPoint mapCenterPoint = new MapPoint(-118.805, 34.027, SpatialReferences.Wgs84);
             MainMapView.SetViewpoint(new Viewpoint(mapCenterPoint, 100000));
+        }
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                string userMessage = MessageText;
+                // Call your Dialogflow and webhook services with userMessage here
+                MessageText = string.Empty;
+            }
         }
     }
 }
